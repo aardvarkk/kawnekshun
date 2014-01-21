c6e5e1ae1853640c803152259f5926181ad4fa1e
aardvarkk <clarksonian@gmail.com>
2014-01-21 14:59:39 -0500

    Upgrading to Rails v4

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
-run Kawnekshun::Application
+run Rails.application
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kawnekshun::Application.config.secret_token = '43f85c927bce47298bfc1455e8933a4ccd5a5d169e666bcea74414008e7348c017858d597c2499625f6d219029c9b032abe88bdac688056c63d3156e4210375c'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kawnekshun::Application.config.secret_key_base = 'e1be69611f17dd1f0a1884e334453d8e3a2b70a7546b25de5a100bbdd3478593560ed6ecbc5804911d509e51c03282400b1bd8ed5469e59d0b1e828a78c86063'
