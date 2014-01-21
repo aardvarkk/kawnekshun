bb4dc95f36f54453586a22aea12ad63f85105dff
aardvarkk <clarksonian@gmail.com>
2014-01-21 17:32:01 -0500

    Got basic parsing of request going.

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,17 @@
+class RankingController < ApplicationController
+
+  # YTO-ATL-CVG/DTT/MEM/MSP/SLC/LAX/SAN/PDX/SFO/SEA-HNL_YYZ-YUL/YOW-YHZ
+  def get
+    routes = params[:routes]
+
+    options = []
+
+    fromto = routes.split('_')
+    fromto.each do |ft|
+      paths = ft.split('-').map{ |s| s.split('/') }
+      options.concat(paths[0].product(*paths[1..-1]))
+    end
+    # throw options
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Kawnekshun::Application.routes.draw do
+  get 'ranking', to: 'ranking#get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
