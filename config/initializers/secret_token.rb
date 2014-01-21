b0f9d9b52844be49292145b730a419cd620caceb
aardvarkk <clarksonian@gmail.com>
2014-01-21 14:15:36 -0500

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kawnekshun::Application.config.secret_token = '43f85c927bce47298bfc1455e8933a4ccd5a5d169e666bcea74414008e7348c017858d597c2499625f6d219029c9b032abe88bdac688056c63d3156e4210375c'
